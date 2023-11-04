482834c4ba4996ae3aeb444194750f3a37119e6e
Cyese <87186561+Cyese@users.noreply.github.com>
2023-11-04 10:26:24 +0700

    modify handler

--- Assignment1/server_lib/handler.py
+++ Assignment1/server_lib/handler.py
@@ -17,58 +17,44 @@ class Service:
                     file.write(log)
                 break
             log += msg + '\n'
+        
 
-
-class Server:
-    def __init__(self, config: dict = json.load(open("config.json", "r"))) -> None:
-        self.storage : Storage = Storage()
+class Handler:
+    def __init__(self) -> None:
+        config: dict = json.load(open("config.json", "r"))
         self.maximiumClient : int = int(config["client"])
         self.port : int = int(config["port"])
         self.thread : list[threading.Thread] = list()
         self.ip : str = config["ip"]
-        self.terminate = True
+        self.online = True
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((self.ip, self.port))
         self.server.listen()
-        self.run()        
-
-    
-    def terminated(self): 
-        # data : dict = json.load(open("config.json", "r"))
-        # data.update("maximiumClient", self.maximiumClient)
-        # data.update("port", self.port)
-        # data.update("ip", self.ip)
-        # json.dump(data, open("config.json", "w+"))
-        pass
+        self.run()
 
     def run(self):
-        while self.terminate: 
-        #     # Aunthenticate upcoming request, assign a worker to that port
-        #     while threading.active_count() <= self.maximiumClient:
-            connection, address = self.server.accept()
-            data = json.loads(connection.recv(1024).decode())
-            if self.aunthenticate(data):
-                data = json.dumps({"connection" : "allow" , "type" : "authorize"})
-                connection.send(data.encode())
-                new_worker = threading.Thread(target=Service, args=(address, connection))
-                new_worker.daemon = False
-                self.thread.append(new_worker)
-                new_worker.start()
-            else:
-                data = json.dumps({"connection" : "refuse" , "type" : "unauthorize"})
-                connection.send(data.encode())
-                connection.close()
-            # if input() == '0':
-            #     break
-            # Check if old thread is still working
-        #     for thread in self.thread:
-        #         if not thread.is_alive():
-        #             self.thread.remove(thread)
-        # for thread in self.thread:
-        #     thread.join()
-        # return
+        while self.online: 
+            while len(self.thread) <= self.maximiumClient:
+                # Aunthenticate upcoming request, assign a worker to that port
+                connection, address = self.server.accept()
+                data = json.loads(connection.recv(1024).decode())
+                if self.aunthenticate(data, address):
+                    data = json.dumps({"connection" : "allow" , "type" : "authorize"})
+                    connection.send(data.encode())
+                    new_worker = threading.Thread(target=Service, args=(address, connection))
+                    new_worker.daemon = False
+                    self.thread.append(new_worker)
+                    new_worker.start()
+                else:
+                    data = json.dumps({"connection" : "refuse" , "type" : "unauthorize"})
+                    connection.send(data.encode())
+                    connection.close()
+                for thread in self.thread:
+                    if not thread.is_alive():
+                        self.thread.remove(thread)
+        return
 
-    def aunthenticate(self, data : dict):
+    def aunthenticate(self, data : dict) -> bool:
         auth : bool
         meh = data["auth"]
         print(meh)
@@ -78,12 +64,26 @@ class Server:
             auth = self.storage.signin(data)
         return auth
 
-
-class ServerInteract:
-    def __init__(self) -> None:
+    def __del__(self): 
+        for thread in self.thread:
+            thread.join()
+            
+    def dsth(self):
         pass
     
-    def run(self):
+class Server:
+    def __init__(self) -> None:
+        # Init Database
+        self.storage : Storage = Storage()
+        # Init background 
+        self.handler : threading.Thread = threading.Thread(target=Handler)
+        
+              
+    def __del__(self): 
+        for thread in self.thread:
+            thread.join()
+        del self.storage
+        del self.handler
         pass
 
     def ping(self, IP: str):
@@ -100,4 +100,12 @@ class ServerInteract:
 
     def allow(self, IP: str):
         pass
+   
+
+
+class ServerInteract:
+    def __init__(self) -> None:
+        pass
+    
+
     # def 
